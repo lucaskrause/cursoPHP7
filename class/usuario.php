10d0d86e9aa0a6e6284ef7d755218c60cbd5b7bf
lucaskrause <37541622+lucaskrause@users.noreply.github.com>
2019-01-07 00:29:40 -0200

    construct, insert, update e delete

--- class/usuario.php
+++ class/usuario.php
@@ -48,14 +48,94 @@ class Usuario {
 
 		// if(isset($results[0])) {
 		if (count($results) > 0) {
-			$row = $results[0];
-
-			$this->setIdUsuario($row['idusuario']);
-			$this->setLogin($row['deslogin']);
-			$this->setSenha($row['dessenha']);
-			$this->setDtCadastro(new DateTime($row['dtcadastro']));
+			$this->setData($results[0]);
 		}
 
+	}
+
+	public static function getList() {
+		$db = new Database();
+
+		return $db->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+	}
+
+	public static function search($login) {
+		$db = new Database();
+
+		return $db->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login ."%"
+		));
+	}
+
+	public function login($login, $senha) {
+		$db = new Database();
+
+		$results = $db->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :SENHA", array(
+			":LOGIN"=>$login,
+			":SENHA"=>$senha
+		));
+
+		// if(isset($results[0])) {
+		if (count($results) > 0) {
+	
+			$this->setData($results[0]);
+			
+		} else {
+			throw new Exception("Login e/ou senha inválidos!");			
+		}
+	}
+
+	public function setData($data) {
+		$this->setIdUsuario($data['idusuario']);
+		$this->setLogin($data['deslogin']);
+		$this->setSenha($data['dessenha']);
+		$this->setDtCadastro(new DateTime($data['dtcadastro']));
+	}
+
+	public function insert() {
+
+		$db = new Database();
+
+		$result = $db->select("CALL sp_usuarios_insert(:LOGIN, :PASSOWORD)", array(
+			':LOGIN'=>$this->getLogin(),
+			':PASSOWORD'=>$this->getSenha()
+		));
+
+		if (count($result) > 0) {
+			$this->setData($result[0]);
+		}
+	}
+
+	public function update($login, $password) {
+		$this->setLogin($login);
+		$this->setSenha($password);
+
+		$db = new Database();
+		
+		$db->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+			':LOGIN'=>$this->getLogin(),
+			':PASSWORD'=>$this->getSenha(),
+			':ID'=>$this->getIdUsuario()
+		));
+
+	}
+
+	public function delete() {
+		$db = new Database();
+
+		$db->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+			':ID'=>$this->getIdUsuario()
+		));
+
+		$this->setIdUsuario(0);
+		$this->setLogin("");
+		$this->setSenha("");
+		$this->setDtCadastro(new DateTime());
+	}
+
+	public function __construct($login="", $password="") {
+		$this->setLogin($login);
+		$this->setSenha($password);
 	}
 
 	public function __toString() {
--- index.php
+++ index.php
@@ -1,18 +1,92 @@
 <?php
 
 require_once("config.php");
+
 /*
+SELECT - SELECIONA OS USUARIOS DA TABELA
+
 $database = new Database();
 
 $usuarios = $database->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
-*/
+
+==============================================================
+CARREGA APENAS UM USUARIO BUSCANDO PELO ID
 
 $user = new Usuario();
 
 $user->loadById(2);
 
 echo $user;
+
+==============================================================
+CARREGA A LISTA DE USUARIO
+
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+
+==============================================================
+CARREGA UMA LISTA DE USUARIO BUSCANDO PELO LOGIN
+
+$search = Usuario::search("u");
+
+echo json_encode($search);
+
+==============================================================
+VALIDACAO DE LOGIN
+
+$usuario = new Usuario();
+$usuario->login("krause", "210797");
+
+echo $usuario;
+
+==============================================================
+INSERT USUARIO - INSERI USUARIO NA TABELA
+---------------------------------------
+insert SEM __construct($login, $senha)
+---------------------------------------
+
+$aluno = new Usuario();
+
+$aluno->setLogin("aluno");
+$aluno->setSenha("@luno");
+
+$aluno->insert();
+
+echo $aluno;
+
+---------------------------------------
+insert COM __construct($login, $senha)
+---------------------------------------
+
+$aluno = new Usuario("aluno", "@luno");
+
+$aluno->insert();
+
+echo $aluno;
+
+==============================================================
+UPDATE - ALTERAR USUÁRIO
+
+$usuario = new Usuario();
+
+$usuario->loadById(4);
+
+$usuario->update("professor", "master");
+
+echo $usuario;
+
+==============================================================
+DELETE - DELETAR USUÁRIO
+*/
+$usuario = new Usuario();
+
+$usuario->loadById(6);
+
+$usuario->delete();
+
+echo $usuario;
 
 ?>
